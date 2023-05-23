dc44611753c131726986c43bafbc35aca8e90f8b
lezcano <lezcano-93@hotmail.com>
2023-05-23 15:59:37 +0000

    Fix SCRIPT_DIR

--- pytorch-build.sh
+++ pytorch-build.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 set -e
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 eval "$(conda shell.bash hook)"
 conda activate pytorch-dev
 cd ~/git/pytorch
 
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source $SCRIPT_DIR/torch-common.sh
 
 pip uninstall torch -y
